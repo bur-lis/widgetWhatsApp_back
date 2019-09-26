d30b6de792437d36627b4dde3250dd444fb53c62
Lisenok <C:\Users\byrak\AppData\Roaming\The Bat!>
2019-09-26 09:38:44 +0300

    Fix connect to passwords

--- init/wa_db_init.php
+++ init/wa_db_init.php
@@ -1,6 +1,6 @@
 <?php
-
 require __DIR__ . '/Database.php';
-include __DIR__ . '../passwords.php';
+require __DIR__ . '/../passwords.php';
+
 $Database_wa = new Database( $DB_wapp['name'], $DB_wapp['pass'], $DB_wapp['host'], $DB_wapp['last']);
 $Database_req = new Database( $DB_leads['name'], $DB_leads['pass'], $DB_leads['host'], $DB_leads['last']);
